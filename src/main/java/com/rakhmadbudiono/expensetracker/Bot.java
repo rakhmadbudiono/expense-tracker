b046f9906835fb7d0be2b7fad011b503efc25372
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2023-12-28 13:45:37 +0700

    chore: setup project

---
+++ src/main/java/com/rakhmadbudiono/expensetracker/Bot.java
@@ -0,0 +1,45 @@
+package com.rakhmadbudiono.expensetracker;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return System.getenv("BOT_USERNAME");
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("BOT_TOKEN");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String chatId = update.getMessage().getChatId().toString();
+            String messageText = update.getMessage().getText();
+
+            if ("/start".equals(messageText)) {
+                sendSimpleMessage(chatId, "Hello! I am your Telegram bot.");
+            }
+        }
+    }
+
+    private void sendSimpleMessage(String chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
